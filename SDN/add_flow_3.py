4ba12707ed94a397f5c678e9fafadac66e81d061
Nguyễn Bảo Phương <yuyavsyuma@gmail.com>
2023-09-06 17:58:57 +0700

    Update add_flow_3.py

--- SDN/add_flow_3.py
+++ SDN/add_flow_3.py
@@ -2,58 +2,6 @@
 
 import requests
 
-## add table
-s1_group50 = {
-    "dpid": 1,
-    "type": "SELECT",
-    "group_id": 50,
-    "buckets": [
-        {
-            "weight": 50,
-            "actions": [
-                {
-                    "type": "OUTPUT",
-                    "port": 1
-                }
-            ]
-        },
-        {
-            "weight": 50,
-             "actions": [
-                {
-                    "type": "OUTPUT",
-                    "port": 2
-                }
-            ]
-        }
-    ]
-}
-
-s3_group53 = {
-    "dpid": 2,
-    "type": "SELECT",
-    "group_id": 51,
-    "buckets": [
-        {
-            "weight": 50,
-            "actions": [
-                {
-                    "type": "OUTPUT",
-                    "port": 1
-                }
-            ]
-        },
-        {
-            "weight": 50,
-             "actions": [
-                {
-                    "type": "OUTPUT",
-                    "port": 2
-                }
-            ]
-        }
-    ]
-}
 
 
 list_group = [s1_group50, s5_group51]
@@ -111,8 +59,8 @@ s1_flow3 = {
     },
     "actions":[
         {
-            "type":"GROUP",
-            "group_id": 50
+            "type":"OUTPUT",
+            "port": 1
         }
     ]
 }
@@ -188,33 +136,14 @@ s3_flow2 = {
     ]
 }
 
-
-
-s4_flow1 = {
-    "dpid": 4,
+s1_flow3 = {
+    "dpid": 1,
     "table_id": 0,
     "idle_timeout": 0,
     "hard_timeout": 0,
     "priority": 100,
     "match":{
-        "in_port":1
-    },
-    "actions":[
-        {
-            "type":"OUTPUT",
-            "port": 2
-        }
-    ]
-}
-
-s4_flow2 = {
-    "dpid": 4,
-    "table_id": 0,
-    "idle_timeout": 0,
-    "hard_timeout": 0,
-    "priority": 100,
-    "match":{
-        "in_port":2
+        "in_port":3
     },
     "actions":[
         {
@@ -225,63 +154,13 @@ s4_flow2 = {
 }
 
 
-s5_flow1 = {
-    "dpid": 5,
-    "table_id": 0,
-    "idle_timeout": 0,
-    "hard_timeout": 0,
-    "priority": 100,
-    "match":{
-        "in_port":1
-    },
-    "actions":[
-        {
-            "type":"OUTPUT",
-            "port": 3
-        }
-    ]
-}
-
-s5_flow2 = {
-    "dpid": 5,
-    "table_id": 0,
-    "idle_timeout": 0,
-    "hard_timeout": 0,
-    "priority": 100,
-    "match":{
-        "in_port":2
-    },
-    "actions":[
-        {
-            "type":"OUTPUT",
-            "port": 3
-        }
-    ]
-}
-
-s5_flow3 = {
-    "dpid": 5,
-    "table_id": 0,
-    "idle_timeout": 0,
-    "hard_timeout": 0,
-    "priority": 100,
-    "match":{
-        "in_port":3
-    },
-    "actions":[
-        {
-            "type":"GROUP",
-            "group_id": 51
-        }
-    ]
-}
 
 
 
 url = "http://localhost:8080/stats/flowentry/add"  # Replace with the appropriate URL
 
 ## list of all flows
-list_flow = [s1_flow1, s1_flow2, s1_flow3, s2_flow1, s2_flow2, s3_flow1, s3_flow2, s4_flow1, s4_flow2, s5_flow1, s5_flow2, s5_flow3]
+list_flow = [s1_flow1, s1_flow2, s1_flow3, s2_flow1, s2_flow2, s3_flow1, s3_flow2]
 for flow in list_flow:
     flow_data = flow
     response = requests.post(url, json=flow_data)
